0f322fbb3516aaabff7310463909624dc4c30c59
Vincenzo Florio <vincenzo.florio@wbdcontractor.com>
2026-07-14 17:55:22 +0200

    introduction

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'discoveryinc-dtc', // Usually your GitHub org/user name.
+  projectName: 'hyoga-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -50,7 +50,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/discoveryinc-dtc/hyoga-docs/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -81,7 +81,7 @@ const config = {
             label: 'Documentation',
           },
           {
-            href: 'https://github.com/discoveryinc-dtc/hyoga-docs',
+            href: 'https://github.com/discoveryinc-dtc/Hyoga',
             label: 'GitHub',
             position: 'right',
           },
